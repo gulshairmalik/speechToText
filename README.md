028e5272bb93914f2b1d07cba459cb5dc440ace3
gulshairmalik <gulshairmalik@gmail.com>
2019-05-26 06:11:59 +0500

    v1.0

---
+++ README.md
@@ -0,0 +1,2 @@
+# Audio Speech to Text
+A simple app developed in Express.js. It takes an audio speech file and converts it into text using Google Speech-to-Text API. You need Google API Credentials to use this app.
---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const path = require('path');
+const speech = require('@google-cloud/speech');
+const fs = require('fs');
+
+// Creates a client
+const client = new speech.SpeechClient();
+  
+// The name of the audio file to transcribe
+const fileName = './resources/audio.wav';
+  
+// Reads a local audio file and converts it to base64
+const file = fs.readFileSync(fileName);
+const audioBytes = file.toString('base64');
+  
+// The audio file's encoding, sample rate in hertz, and BCP-47 language code
+const audio = {
+    content: audioBytes,
+};
+const config = {
+    encoding: 'LINEAR16',
+    sampleRateHertz: 16000,
+    languageCode: 'ur-PK',
+};
+const request = {
+    audio: audio,
+    config: config,
+};
+  
+const app = express();
+
+async function sendRequest(){
+
+  // Detects speech in the audio file
+  const [response] = await client.recognize(request);
+  const transcription = response.results.map(result => result.alternatives[0].transcript).join('\n');
+
+  return transcription;
+}
+
+app.get('/',(req,res)=>{
+
+    sendRequest().then(value =>{
+      res.send(value);
+    });
+
+});
+
+
+
+app.listen(port, () => {
+    console.log('Server started on port '+port);
+});
+
+  
